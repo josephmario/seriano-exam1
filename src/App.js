a65777f0f6a854fa2504d0e46c11852a60443285
unknown <josephmario_santiago@yahoo.com>
2024-03-18 13:33:58 +0800

    finish exam 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
 
-function App() {
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.todo];
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const TodoList = () => {
+  const [todos, dispatch] = useReducer(todosReducer, []);
+
+  const addTodo = () => {
+    const newTodo = {
+      id: Math.random().toString(36).substr(2, 9), // Generate a unique id for the todo
+      text: `Todo ${todos.length + 1}`
+    };
+    dispatch({ type: 'ADD_TODO', todo: newTodo });
+  };
+
+  const removeTodo = (id) => {
+    dispatch({ type: 'REMOVE_TODO', id });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Remove todo</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={addTodo}>Add todo</button>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
