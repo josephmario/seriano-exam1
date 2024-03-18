27d84cf8c283ca39aa3351d51523c4cd9cc126d1
unknown <josephmario_santiago@yahoo.com>
2024-03-18 13:39:39 +0800

    finish exam3

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const todosReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [...state, action.todo];
-    case 'REMOVE_TODO':
-      return state.filter(todo => todo.id !== action.id);
-    default:
-      return state;
+const initialData = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "status": 1
+  },
+  {
+    "id": 2,
+    "name": "Jane Doe",
+    "status": 2
+  },
+  {
+    "id": 3,
+    "name": "Adam Rocket",
+    "status": 2
+  },
+  {
+    "id": 4,
+    "name": "Luis Rocket",
+    "status": 1
   }
+];
+
+const useGroupByStatus = (data) => {
+  const [groupedData, setGroupedData] = useState({});
+
+  useEffect(() => {
+    const result = {};
+    data.forEach(item => {
+      const key = `status-${item.status}`;
+      if (!result[key]) {
+        result[key] = [];
+      }
+      result[key].push(item);
+    });
+    setGroupedData(result);
+  }, [data]);
+
+  return groupedData;
 };
 
-const TodoList = () => {
-  const [todos, dispatch] = useReducer(todosReducer, []);
+const MyComponent = () => {
+  const groupedData = useGroupByStatus(initialData);
 
-  const addTodo = () => {
-    const newTodo = {
-      id: Math.random().toString(36).substr(2, 9), // Generate a unique id for the todo
-      text: `Todo ${todos.length + 1}`
-    };
-    dispatch({ type: 'ADD_TODO', todo: newTodo });
-  };
-
-  const removeTodo = (id) => {
-    dispatch({ type: 'REMOVE_TODO', id });
-  };
+  useEffect(() => {
+    console.log(groupedData);
+  }, [groupedData]);
 
   return (
     <div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => removeTodo(todo.id)}>Remove todo</button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={addTodo}>Add todo</button>
+      {/* Render your component UI here */}
     </div>
   );
 };
 
-export default TodoList;
+export default MyComponent;
